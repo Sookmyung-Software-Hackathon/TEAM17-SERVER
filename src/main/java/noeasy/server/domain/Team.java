cd6d11d6e3a49583a0d3253eab5adbfd3afa64e5
YEJIN <yejin9858@sookmyung.ac.kr>
2022-08-27 18:48:56 +0900

    [FEAT] 멤버 추가

--- src/main/java/noeasy/server/domain/Team.java
+++ src/main/java/noeasy/server/domain/Team.java
@@ -1,6 +1,18 @@
 package noeasy.server.domain;
 
-import javax.persistence.Entity;
+import noeasy.server.util.RandomGenerator;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Team extends Timestamped {
+    @Id
+    @GeneratedValue(generator = RandomGenerator.generatorName)
+    @GenericGenerator(name = RandomGenerator.generatorName, strategy = "com.noeasy.server.util.RandomGenerator")
+    @Column
+    private String id;
 }
